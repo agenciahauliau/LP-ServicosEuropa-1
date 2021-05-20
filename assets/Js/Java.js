253d943ed6d3db9a19491f1371dda7ce868aeeaf
Itamar <itamar@hi4u.me>
2021-05-20 16:49:13 -0300

    Mobile final

--- assets/Js/Java.js
+++ assets/Js/Java.js
@@ -47,6 +47,7 @@ function scrollSection() {
     }
   }
 }
+
 const services = document.querySelectorAll(".boxServices div[class*='Box']");
 
 for (const idx in services) {
@@ -57,6 +58,21 @@ for (const idx in services) {
     scrollTwo.scrollTo({
       top: 0,
       left: (idx * services[0].offsetWidth) - 40,
+      behavior: 'smooth'
+    });
+  };
+}
+
+const pickMes = document.querySelectorAll(".textInformation div[class*='Text'] .text button");
+const package = document.querySelector("select[name='package']");
+const form = document.querySelector(".five .form form");
+
+for (const idx in pickMes) {
+
+  pickMes[idx].onclick =  function () {
+    package.value = pickMes[idx].value;
+    window.scrollTo({
+      top: form.offsetTop,
       behavior: 'smooth'
     });
   };
